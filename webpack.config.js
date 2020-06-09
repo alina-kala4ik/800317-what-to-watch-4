8c4c3217837bda6db886fcd72b59cddc65c0c5ad
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-09 14:31:41 +0300

    Настраивает сборку и линтинг проекта

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    path: path.resolve(__dirname, `public`),
+    filename: `bundle.js`
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: true,
+    port: 1337
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`
+        },
+      },
+    ]
+  },
+  devtool: `sourse-map`
+};
