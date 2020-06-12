594211601a5bedc76592d85720aea32c6bf8d80c
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-12 10:48:46 +0300

    Добавляет propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from 'prop-types';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {promotionTitle, promotionGenre, promotionReleaseDate} = props;
+  const {promotionTitle, promotionGenre, promotionReleaseDate, films} = props;
 
   return <Main
     promotionTitle={promotionTitle}
     promotionGenre={promotionGenre}
     promotionReleaseDate={promotionReleaseDate}
+    films={films}
   />;
+};
+
+App.propTypes = {
+  promotionTitle: PropTypes.string.isRequired,
+  promotionGenre: PropTypes.string.isRequired,
+  promotionReleaseDate: PropTypes.string.isRequired,
+  films: PropTypes.arrayOf(
+      PropTypes.string.isRequired
+  ).isRequired,
 };
 
 export default App;
