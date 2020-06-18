74ead8a204c6055eb663af6a6678c68b8c235860
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-18 14:38:54 +0300

    Обновляет тесты

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -8,9 +8,48 @@ const settings = {
   promotionReleaseDate: `2014`,
 };
 
-const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
-
-const onFilmTitleClick = () => {};
+const films = [
+  {
+    id: 1,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  },
+  {
+    id: 2,
+    title: `Bohemian Rhapsody`,
+    src: `img/bohemian-rhapsody.jpg`,
+  },
+  {
+    id: 3,
+    title: `Macbeth`,
+    src: `img/macbeth.jpg`
+  },
+  {
+    id: 4,
+    title: `Aviator`,
+    src: `img/aviator.jpg`,
+  },
+  {
+    id: 5,
+    title: `We need to talk about Kevin`,
+    src: `img/we-need-to-talk-about-kevin.jpg`,
+  },
+  {
+    id: 6,
+    title: `What We Do in the Shadows`,
+    src: `img/what-we-do-in-the-shadows.jpg`,
+  },
+  {
+    id: 7,
+    title: `Revenant`,
+    src: `img/revenant.jpg`,
+  },
+  {
+    id: 8,
+    title: `Johnny English`,
+    src: `img/johnny-english.jpg`,
+  },
+];
 
 it(`render Main`, () => {
   const tree = renderer
@@ -19,7 +58,7 @@ it(`render Main`, () => {
       promotionGenre={settings.promotionGenre}
       promotionReleaseDate={settings.promotionReleaseDate}
       films={films}
-      onFilmTitleClick={onFilmTitleClick}
+      onFilmTitleClick={()=>{}}
     />)
     .toJSON();
   expect(tree).toMatchSnapshot();
