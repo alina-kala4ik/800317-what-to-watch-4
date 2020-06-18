7791a0f79fd4687e684a7101d299839cfc8e1968
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-18 14:47:19 +0300

    Добавляет снэпшот тесты к новым компонентам

---
+++ src/components/movie-card/movie-card.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import MoveCard from "./movie-card.jsx";
+
+const film = {
+  id: 1,
+  title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+};
+
+it(`render MoveCard`, () => {
+  const tree = renderer
+    .create(<MoveCard
+      film={film}
+      onCardActive={()=>{}}
+      onFilmTitleClick={()=>{}}
+    />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
