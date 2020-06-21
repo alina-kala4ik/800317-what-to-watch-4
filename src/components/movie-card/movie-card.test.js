6ae9aaeb6e385c973886e63aa57fb728b5c00bb1
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-21 10:12:30 +0300

    Обновляет тесты

--- src/components/movie-card/movie-card.test.js
+++ src/components/movie-card/movie-card.test.js
@@ -5,6 +5,15 @@ import MoveCard from "./movie-card.jsx";
 const film = {
   title: `Fantastic Beasts: The Crimes of Grindelwald`,
   src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  poster: `img/bg-the-grand-budapest-hotel.jpg`,
+  movieCover: `img/the-grand-budapest-hotel-poster.jpg`,
+  genre: `Drama`,
+  yearRelease: `2017`,
+  description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  rating: `5,6`,
+  numberVotes: `278`,
+  producer: `Wes Andreson`,
+  actors: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`],
 };
 
 it(`render MoveCard`, () => {
