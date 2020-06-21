3067be936fff08c5bbffe042b29565497826ac2d
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-21 10:46:25 +0300

    Добавляет тест который валидирует значения, передаваемые через props.

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -146,3 +146,37 @@ it(`Film title or image click`, () => {
 
   expect(onFilmTitleOrImgClick).toHaveBeenCalledTimes(16);
 });
+
+it(`Validates data transmitted through props when clicked`, () => {
+  const onFilmTitleOrImgClick = jest.fn();
+  const expectedData = {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    src: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    poster: `img/bg-the-grand-budapest-hotel.jpg`,
+    movieCover: `img/the-grand-budapest-hotel-poster.jpg`,
+    genre: `Drama`,
+    yearRelease: `2017`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+    rating: `5,6`,
+    numberVotes: `278`,
+    producer: `Wes Andreson`,
+    actors: [`Bill Murray`, `Edward Norton`, `Jude Law`, `Willem Dafoe`]
+  };
+
+  const main = mount(
+      <Main
+        promotionTitle={settings.promotionTitle}
+        promotionGenre={settings.promotionGenre}
+        promotionReleaseDate={settings.promotionReleaseDate}
+        films={films}
+        onFilmTitleOrImgClick={onFilmTitleOrImgClick}
+      />
+
+  );
+
+  const filmOneTitles = main.find(`a.small-movie-card__link`).at(0);
+
+  filmOneTitles.simulate(`click`);
+
+  expect(onFilmTitleOrImgClick.mock.calls[0][0]).toMatchObject(expectedData);
+});
