8246df375c513759305c1b325df85dcd6d1a84f6
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-24 16:43:16 +0300

    Добавляет e2e тест для VideoPlayer

---
+++ src/components/video-player/video-player.e2e.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import VideoPlayer from "./video-player.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+it(`The component has a playback state`, () => {
+  const expectedState = {isPlaying: true};
+
+  const videoPlayer = mount(
+      <VideoPlayer
+        videoPreview={`https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`}
+        src={`img/fantastic-beasts-the-crimes-of-grindelwald.jpg`}
+        isPlaying={true}
+      />
+  );
+
+  const state = videoPlayer.state();
+
+  expect(state).toMatchObject(expectedState);
+});
+
+it(`The component has a pause state`, () => {
+  const expectedState = {isPlaying: false};
+
+  const videoPlayer = mount(
+      <VideoPlayer
+        videoPreview={`https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`}
+        src={`img/fantastic-beasts-the-crimes-of-grindelwald.jpg`}
+        isPlaying={false}
+      />
+  );
+
+  const state = videoPlayer.state();
+
+  expect(state).toMatchObject(expectedState);
+});
