8bd2c802adbd9032fbe4e0eee0a0f0de36a3124b
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-24 16:25:18 +0300

    Добавляет снэпшот для VideoPlayer

---
+++ src/components/video-player/video-player.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import VideoPlayer from "./video-player.jsx";
+
+it(`render VideoPlayer`, () => {
+  const tree = renderer
+    .create(<VideoPlayer
+      videoPreview={`https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`}
+      src={`img/fantastic-beasts-the-crimes-of-grindelwald.jpg`}
+      isPlaying={false}
+    />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
