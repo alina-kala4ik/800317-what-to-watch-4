6467593b2b39883bd3a39feff40ff9932a22cd13
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-25 15:07:41 +0300

    Добавляет блок «More like this» в компоненте MoviePage

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,9 +8,25 @@ import MoviePage from "./../movie-page/movie-page.jsx";
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = {
       selectedFilm: null,
     };
+
+    this.handleFilmTitleClick = this.handleFilmTitleClick.bind(this);
+    this.handleFilmImgClick = this.handleFilmImgClick.bind(this);
+  }
+
+  handleFilmTitleClick(film) {
+    this.setState({
+      selectedFilm: film
+    });
+  }
+
+  handleFilmImgClick(film) {
+    this.setState({
+      selectedFilm: film
+    });
   }
 
   _renderApp() {
@@ -22,20 +38,17 @@ class App extends PureComponent {
         promotionGenre={promotionGenre}
         promotionReleaseDate={promotionReleaseDate}
         films={films}
-        onFilmTitleClick={(film)=> {
-          this.setState({
-            selectedFilm: film
-          });
-        }}
-        onFilmImgClick={(film)=> {
-          this.setState({
-            selectedFilm: film
-          });
-        }}
+        onFilmTitleClick={this.handleFilmTitleClick}
+        onFilmImgClick={this.handleFilmImgClick}
       />;
     }
 
-    return <MoviePage film={this.state.selectedFilm}/>;
+    return <MoviePage
+      film={this.state.selectedFilm}
+      films={films}
+      onFilmTitleClick={this.handleFilmTitleClick}
+      onFilmImgClick={this.handleFilmImgClick}
+    />;
   }
 
   render() {
@@ -47,7 +60,12 @@ class App extends PureComponent {
           {this._renderApp()}
         </Route>
         <Route exact path="/movie-page">
-          <MoviePage film={films[0]}/>
+          <MoviePage
+            film={films[0]}
+            films={films}
+            onFilmTitleClick={this.handleFilmTitleClick}
+            onFilmImgClick={this.handleFilmImgClick}
+          />
         </Route>
       </Switch>
     </BrowserRouter>;
--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
 import Tabs from "./../tabs/tabs.jsx";
+import MovieList from "./../movie-list/movie-list.jsx";
 
 const getRatingLevel = (rating) => {
   const formattedRating = parseFloat(rating.replace(`,`, `.`));
@@ -46,97 +47,132 @@ class MoviePage extends PureComponent {
     });
   }
 
+  getRelatedMovies(genre) {
+    const {films} = this.props;
+    return films.filter((film)=>film.genre === genre).slice(4);
+  }
+
   render() {
-    const {film} = this.props;
+    const {film, onFilmTitleClick, onFilmImgClick} = this.props;
     const {title, poster, movieCover, genre, yearRelease, description, rating, numberVotes, producer, actors} = film;
     const ratingLevel = getRatingLevel(rating);
     const listTabs = Object.values(TABS);
+    const relatedMovies = this.getRelatedMovies(genre);
 
-    return <section className="movie-card movie-card--full">
-      <div className="movie-card__hero">
-        <div className="movie-card__bg">
-          <img src={poster} alt={title} />
+    return <React.Fragment>
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__hero">
+          <div className="movie-card__bg">
+            <img src={poster} alt={title} />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <header className="page-header movie-card__head">
+            <div className="logo">
+              <a href="main.html" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </div>
+          </header>
+
+          <div className="movie-card__wrap">
+            <div className="movie-card__desc">
+              <h2 className="movie-card__title">{title}</h2>
+              <p className="movie-card__meta">
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{yearRelease}</span>
+              </p>
+
+              <div className="movie-card__buttons">
+                <button className="btn btn--play movie-card__button" type="button">
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s"></use>
+                  </svg>
+                  <span>Play</span>
+                </button>
+                <button className="btn btn--list movie-card__button" type="button">
+                  <svg viewBox="0 0 19 20" width="19" height="20">
+                    <use xlinkHref="#add"></use>
+                  </svg>
+                  <span>My list</span>
+                </button>
+                <a href="add-review.html" className="btn movie-card__button">Add review</a>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <h1 className="visually-hidden">WTW</h1>
+        <div className="movie-card__wrap movie-card__translate-top">
+          <div className="movie-card__info">
+            <div className="movie-card__poster movie-card__poster--big">
+              <img src={movieCover} alt={title} width="218" height="327" />
+            </div>
 
-        <header className="page-header movie-card__head">
+            <div className="movie-card__desc">
+              <nav className="movie-nav movie-card__nav">
+                {<Tabs
+                  activeTab={this.state.activeTab}
+                  listTabs={listTabs}
+                  onTabClick={this.handleTabClick}
+                />}
+              </nav>
+
+              <div className="movie-rating">
+                <div className="movie-rating__score">{rating}</div>
+                <p className="movie-rating__meta">
+                  <span className="movie-rating__level">{ratingLevel}</span>
+                  <span className="movie-rating__count">{numberVotes} ratings</span>
+                </p>
+              </div>
+
+              <div className="movie-card__text">
+                <p>{description}</p>
+
+                <p className="movie-card__director"><strong>Director: {producer}</strong></p>
+
+                <p className="movie-card__starring"><strong>Starring: {actors.join(`, `)}</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="page-content">
+        <section className="catalog catalog--like-this">
+          <h2 className="catalog__title">More like this</h2>
+
+          <div className="catalog__movies-list">
+            {<MovieList
+              films={relatedMovies}
+              onFilmTitleClick={onFilmTitleClick}
+              onFilmImgClick={onFilmImgClick}
+            />}
+          </div>
+        </section>
+
+        <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link">
+            <a href="main.html" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
             </a>
           </div>
 
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
           </div>
-        </header>
-
-        <div className="movie-card__wrap">
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{title}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{yearRelease}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button">
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
-              </button>
-              <button className="btn btn--list movie-card__button" type="button">
-                <svg viewBox="0 0 19 20" width="19" height="20">
-                  <use xlinkHref="#add"></use>
-                </svg>
-                <span>My list</span>
-              </button>
-              <a href="add-review.html" className="btn movie-card__button">Add review</a>
-            </div>
-          </div>
-        </div>
+        </footer>
       </div>
-
-      <div className="movie-card__wrap movie-card__translate-top">
-        <div className="movie-card__info">
-          <div className="movie-card__poster movie-card__poster--big">
-            <img src={movieCover} alt={title} width="218" height="327" />
-          </div>
-
-          <div className="movie-card__desc">
-            <nav className="movie-nav movie-card__nav">
-              {<Tabs
-                activeTab={this.state.activeTab}
-                listTabs={listTabs}
-                onTabClick={this.handleTabClick}
-              />}
-            </nav>
-
-            <div className="movie-rating">
-              <div className="movie-rating__score">{rating}</div>
-              <p className="movie-rating__meta">
-                <span className="movie-rating__level">{ratingLevel}</span>
-                <span className="movie-rating__count">{numberVotes} ratings</span>
-              </p>
-            </div>
-
-            <div className="movie-card__text">
-              <p>{description}</p>
-
-              <p className="movie-card__director"><strong>Director: {producer}</strong></p>
-
-              <p className="movie-card__starring"><strong>Starring: {actors.join(`, `)}</strong></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>;
+    </React.Fragment>;
   }
 }
 
@@ -152,7 +188,22 @@ MoviePage.propTypes = {
     numberVotes: PropTypes.string.isRequired,
     producer: PropTypes.string.isRequired,
     actors: PropTypes.arrayOf(PropTypes.string).isRequired,
-  }).isRequired
+  }).isRequired,
+  films: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    movieCover: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    yearRelease: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
+    numberVotes: PropTypes.string.isRequired,
+    producer: PropTypes.string.isRequired,
+    actors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  })).isRequired,
+  onFilmTitleClick: PropTypes.func.isRequired,
+  onFilmImgClick: PropTypes.func.isRequired,
 };
 
 export default MoviePage;
