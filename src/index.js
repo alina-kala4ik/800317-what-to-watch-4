530f4469c6a415f890c7865cb98794d2bfc0af36
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-06-27 23:34:47 +0300

    Подключает redux в проект

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
-import films from "./mocks/films.js";
+import {createStore} from "redux";
+import {reducer} from "./reducer.js";
+import {Provider} from "react-redux";
 
 const settings = {
   promotionTitle: `The Grand Budapest Hotel`,
@@ -9,14 +11,20 @@ const settings = {
   promotionReleaseDate: `2014`,
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 const init = () => {
   ReactDOM.render(
-      <App
-        promotionTitle={settings.promotionTitle}
-        promotionGenre={settings.promotionGenre}
-        promotionReleaseDate={settings.promotionReleaseDate}
-        films={films}
-      />,
+      <Provider store={store}>
+        <App
+          promotionTitle={settings.promotionTitle}
+          promotionGenre={settings.promotionGenre}
+          promotionReleaseDate={settings.promotionReleaseDate}
+        />
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
