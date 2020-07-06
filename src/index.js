070ac82a97e5a8849f92222c9e30599907939916
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-07-06 23:22:19 +0300

    Удаляет пропсы содержащие информацию о промо-фильме
    Удаляет пропсы содержащие информацию о промо-фильме т.к. промо фильм должен рендерится на основе объекта фильм

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import {reducer} from "./reducer.js";
 import {Provider} from "react-redux";
 import withActiveItem from "./hocs/with-active-item/with-active-item.jsx";
 
-const settings = {
-  promotionTitle: `The Grand Budapest Hotel`,
-  promotionGenre: `Drama`,
-  promotionReleaseDate: `2014`,
-};
-
 const store = createStore(
     reducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
@@ -22,11 +16,7 @@ const AppWrapped = withActiveItem(App);
 const init = () => {
   ReactDOM.render(
       <Provider store={store}>
-        <AppWrapped
-          promotionTitle={settings.promotionTitle}
-          promotionGenre={settings.promotionGenre}
-          promotionReleaseDate={settings.promotionReleaseDate}
-        />
+        <AppWrapped />
       </Provider>,
       document.querySelector(`#root`)
   );
