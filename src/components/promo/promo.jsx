6d4ea1e78ff73d128070fdb7b33dba23e6b1b713
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-07-12 21:34:43 +0300

    Модифицирует компонент промо
    1. отображает ссылку для входа или аваратку пользователя в зависимости от того залогинен пользователь или нет
    2. по клику на ссылку для входа состояние приложения logIn изменяется на true и отображается экран для входа
    3. подключает аватарку которую отдает сервер при регистрации

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -1,74 +1,107 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import {connect} from "react-redux";
 import {ActionCreator} from "./../../reducer/app-state/app-state.js";
 import PropTypes from "prop-types";
 import {getPromoFilm} from "./../../reducer/data/selector.js";
+import {getAuthorizationStatus, getAvatar} from "./../../reducer/user/selector.js";
+import {AuthorizationStatus} from "./../../reducer/user/user.js";
 
-const Promo = (props) => {
-  const {onPlayClick, film} = props;
-  const {title, posterSrc, movieCoverSrc, genre, yearRelease} = film;
+class Promo extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return <section className="movie-card">
-    <div className="movie-card__bg">
-      <img src={posterSrc} alt={title} />
-    </div>
+    this.handleSignInClick = this.handleSignInClick.bind(this);
+  }
 
-    <h1 className="visually-hidden">WTW</h1>
+  handleSignInClick(evt) {
+    const {onSignInClick} = this.props;
 
-    <header className="page-header movie-card__head">
-      <div className="logo">
-        <a className="logo__link">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
+    evt.preventDefault();
+
+    onSignInClick();
+  }
+
+  render() {
+    const {onPlayClick, film, authorizationStatus, avatar} = this.props;
+    const {title, posterSrc, movieCoverSrc, genre, yearRelease} = film;
+
+    let userBlock;
+
+    if (authorizationStatus === AuthorizationStatus.AUTH) {
+      userBlock = <div className="user-block__avatar">
+        <img src={avatar} alt="User avatar" width="63" height="63" />
+      </div>;
+    } else if (authorizationStatus === AuthorizationStatus.NO_AUTH) {
+      userBlock = <a
+        href="sign-in.html"
+        className="user-block__link"
+        onClick={this.handleSignInClick}
+      >
+        Sign in
+      </a>;
+    }
+
+    return <section className="movie-card">
+      <div className="movie-card__bg">
+        <img src={posterSrc} alt={title} />
       </div>
 
-      <div className="user-block">
-        <div className="user-block__avatar">
-          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-        </div>
-      </div>
-    </header>
+      <h1 className="visually-hidden">WTW</h1>
 
-    <div className="movie-card__wrap">
-      <div className="movie-card__info">
-        <div className="movie-card__poster">
-          <img src={movieCoverSrc} alt={`${title} poster`} width="218" height="327" />
+      <header className="page-header movie-card__head">
+        <div className="logo">
+          <a className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
         </div>
 
-        <div className="movie-card__desc">
-          <h2 className="movie-card__title">{title}</h2>
-          <p className="movie-card__meta">
-            <span className="movie-card__genre">{genre}</span>
-            <span className="movie-card__year">{yearRelease}</span>
-          </p>
+        <div className="user-block">
+          {userBlock}
+        </div>
+      </header>
 
-          <div className="movie-card__buttons">
-            <button
-              className="btn btn--play movie-card__button"
-              type="button"
-              onClick={()=>{
-                onPlayClick(film);
-              }}
-            >
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s" />
-              </svg>
-              <span>Play</span>
-            </button>
-            <button className="btn btn--list movie-card__button" type="button">
-              <svg viewBox="0 0 19 20" width="19" height="20">
-                <use xlinkHref="#add" />
-              </svg>
-              <span>My list</span>
-            </button>
+      <div className="movie-card__wrap">
+        <div className="movie-card__info">
+          <div className="movie-card__poster">
+            <img src={movieCoverSrc} alt={`${title} poster`} width="218" height="327" />
+          </div>
+
+          <div className="movie-card__desc">
+            <h2 className="movie-card__title">{title}</h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">{genre}</span>
+              <span className="movie-card__year">{yearRelease}</span>
+            </p>
+
+            <div className="movie-card__buttons">
+              <button
+                className="btn btn--play movie-card__button"
+                type="button"
+                onClick={()=>{
+                  onPlayClick(film);
+                }}
+              >
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s" />
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list movie-card__button" type="button">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add" />
+                </svg>
+                <span>My list</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>;
-};
+    </section>;
+  }
+
+}
 
 
 Promo.propTypes = {
@@ -80,19 +113,29 @@ Promo.propTypes = {
     genre: PropTypes.string.isRequired,
     yearRelease: PropTypes.number.isRequired,
   }),
+  authorizationStatus: PropTypes.string.isRequired,
+  onSignInClick: PropTypes.func.isRequired,
+  avatar: PropTypes.string
 };
 
 const mapStateToProps = (state) => {
   const film = getPromoFilm(state);
+  const authorizationStatus = getAuthorizationStatus(state);
+  const avatar = getAvatar(state);
 
   return {
-    film
+    film,
+    authorizationStatus,
+    avatar,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   onPlayClick(film) {
     dispatch(ActionCreator.chooseMovieToWatch(film));
+  },
+  onSignInClick() {
+    dispatch(ActionCreator.logIn(true));
   }
 });
 
