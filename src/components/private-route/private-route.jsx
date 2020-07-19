986af5ed0c5dacca492507e236c7405128b4eef8
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-07-19 20:29:05 +0300

    Добавляет передачу пропа historyProps в приватном компоненте

--- src/components/private-route/private-route.jsx
+++ src/components/private-route/private-route.jsx
@@ -13,10 +13,10 @@ const PrivateRoute = (props) => {
     <Route
       path={path}
       exact={exact}
-      render={() => {
+      render={(historyProps) => {
         return (
           authorizationStatus === AuthorizationStatus.AUTH
-            ? render()
+            ? render(historyProps)
             : <Redirect to={Pages.LOGIN} />
         );
       }}
