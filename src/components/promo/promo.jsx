72cb585850d22e4b5afd8401029415042934a5bb
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-07-23 15:26:40 +0300

    Рефакторинг

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {getPromoFilm} from "./../../reducer/data/selector.js";
@@ -7,64 +7,57 @@ import {Link} from "react-router-dom";
 import MyListButton from "../my-list-button/my-list-button.jsx";
 import {Pages} from "../../utils.js";
 
-class Promo extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const Promo = (props) => {
+  const {film} = props;
+  const {title, posterSrc, movieCoverSrc, genre, yearRelease, isFavorite, id} = film;
 
-  render() {
-    const {film} = this.props;
-    const {title, posterSrc, movieCoverSrc, genre, yearRelease, isFavorite, id} = film;
+  return <section className="movie-card">
+    <div className="movie-card__bg">
+      <img src={posterSrc} alt={title} />
+    </div>
 
-    return <section className="movie-card">
-      <div className="movie-card__bg">
-        <img src={posterSrc} alt={title} />
-      </div>
+    <h1 className="visually-hidden">WTW</h1>
 
-      <h1 className="visually-hidden">WTW</h1>
+    <Header
+      uniqueClasses="movie-card__head"
+      isActiveLogoLink={false}
+    />
 
-      <Header
-        uniqueClasses="movie-card__head"
-        isActiveLogoLink={false}
-      />
+    <div className="movie-card__wrap">
+      <div className="movie-card__info">
+        <div className="movie-card__poster">
+          <img src={movieCoverSrc} alt={`${title} poster`} width="218" height="327" />
+        </div>
 
-      <div className="movie-card__wrap">
-        <div className="movie-card__info">
-          <div className="movie-card__poster">
-            <img src={movieCoverSrc} alt={`${title} poster`} width="218" height="327" />
-          </div>
+        <div className="movie-card__desc">
+          <h2 className="movie-card__title">{title}</h2>
+          <p className="movie-card__meta">
+            <span className="movie-card__genre">{genre}</span>
+            <span className="movie-card__year">{yearRelease}</span>
+          </p>
 
-          <div className="movie-card__desc">
-            <h2 className="movie-card__title">{title}</h2>
-            <p className="movie-card__meta">
-              <span className="movie-card__genre">{genre}</span>
-              <span className="movie-card__year">{yearRelease}</span>
-            </p>
-
-            <div className="movie-card__buttons">
-              <Link
-                className="btn btn--play movie-card__button"
-                type="button"
-                to={Pages.PLAYER.replace(`:id`, id)}
-              >
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s" />
-                </svg>
-                <span>Play</span>
-              </Link>
-              <MyListButton
-                id={id}
-                isFavorite={isFavorite}
-                isPromoFilm={true}
-              />
-            </div>
+          <div className="movie-card__buttons">
+            <Link
+              className="btn btn--play movie-card__button"
+              type="button"
+              to={Pages.PLAYER.replace(`:id`, id)}
+            >
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s" />
+              </svg>
+              <span>Play</span>
+            </Link>
+            <MyListButton
+              id={id}
+              isFavorite={isFavorite}
+              isPromoFilm={true}
+            />
           </div>
         </div>
       </div>
-    </section>;
-  }
-
-}
+    </div>
+  </section>;
+};
 
 
 Promo.propTypes = {
