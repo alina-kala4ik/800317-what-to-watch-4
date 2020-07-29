2b7fb3fd2aa1b98e5f9ec5f3f461ca76a9b2cf9a
Алина Калачева <alinakalaceva@MacBook-Pro-Kirill.local>
2020-07-29 18:17:18 +0300

    Исправляет функцию getRatingLevel

--- src/components/tab-overview/tab-overview.jsx
+++ src/components/tab-overview/tab-overview.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const getRatingLevel = (rating) => {
-  if (rating === 0 || rating < 3) {
+  if (rating >= 0 && rating < 3) {
     return `Bad`;
   }
-  if (rating >= 3 || rating < 5) {
+  if (rating >= 3 && rating < 5) {
     return `Normal`;
   }
-  if (rating >= 5 || rating < 8) {
+  if (rating >= 5 && rating < 8) {
     return `Good`;
   }
-  if (rating >= 8 || rating < 10) {
+  if (rating >= 8 && rating < 10) {
     return `Very good`;
   }
   if (rating >= 10) {
